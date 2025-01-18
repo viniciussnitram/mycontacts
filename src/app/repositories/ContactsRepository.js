b4c46478de3d1f45bc57f245ff7d05a31efddf0e
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-01-17 22:13:18 -0300

    feat: create routes to show (get contact by ID) and delete the contact.

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,19 +1,39 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Vinicius',
     email: 'vinicius@mail.com',
     phone: '121213131',
-    category_id: uuid(),
-  }
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'José',
+    email: 'jose@mail.com',
+    phone: '543648864',
+    category_id: v4(),
+  },
 ];
 
 class ContactsRepository {
   findAll() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
