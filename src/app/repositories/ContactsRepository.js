f28acfb4154484d49ddcd05b05d0bc138a18843b
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-01-28 22:06:06 -0300

    refactor: change the way of update contact. now we are using SQL query to do that

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -40,13 +40,6 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
-  }
-
   async create({
     name,
     email,
@@ -62,7 +55,7 @@ class ContactsRepository {
     return row;
   }
 
-  update(
+  async update(
     id, {
       name,
       email,
@@ -70,20 +63,20 @@ class ContactsRepository {
       category_id
     }
   ) {
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+    `, [name, email, phone, category_id, id]);
+
+    return row;
+  }
+
+  delete(id) {
     return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id
-      };
-
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-
-      resolve(updatedContact);
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
